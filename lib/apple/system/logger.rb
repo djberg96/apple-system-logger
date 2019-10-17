f5ae92aef9159ab163669d39d213052f20d2ac0b
Daniel Berger <djberg96@gmail.com>
2019-10-17 19:42:14 -0400

    Added constants.

--- lib/apple/system/logger.rb
+++ lib/apple/system/logger.rb
@@ -1,9 +1,11 @@
 require_relative 'logger/functions'
+require_relative 'logger/constants'
 
 module Apple
   module System
     class Logger
       include Apple::System::LoggerFunctions
+      include Apple::System::LoggerConstants
     end
   end
 end
