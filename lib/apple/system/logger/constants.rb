d9317f82f37b91a89b016e5597766e8a380a23cd
Daniel Berger <djberg96@gmail.com>
2019-10-18 20:04:51 -0400

    Added level constants.

--- lib/apple/system/logger/constants.rb
+++ lib/apple/system/logger/constants.rb
@@ -10,6 +10,15 @@ module Apple
       ASL_KEY_GID      = "GID"
       ASL_KEY_LEVEL    = "Level"
       ASL_KEY_MSG      = "Message"
+
+      ASL_LEVEL_EMERG   = 0
+      ASL_LEVEL_ALERT   = 1
+      ASL_LEVEL_CRIT    = 2
+      ASL_LEVEL_ERR     = 3
+      ASL_LEVEL_WARNING = 4
+      ASL_LEVEL_NOTICE  = 5
+      ASL_LEVEL_INFO    = 6
+      ASL_LEVEL_DEBUG   = 7
     end
   end
 end
