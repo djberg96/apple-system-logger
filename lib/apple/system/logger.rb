061a8ce628c6b938e88f2ae98c84520b9b30c1bf
Daniel Berger <djberg96@gmail.com>
2019-10-18 21:26:31 -0400

    Add a bunch of methods, set some defaults.

--- lib/apple/system/logger.rb
+++ lib/apple/system/logger.rb
@@ -13,25 +13,63 @@ module Apple
 
       def initialize(**kwargs)
         @facility = kwargs[:facility]
-        @level    = kwargs[:level]
+        @level    = kwargs[:level] || ASL_LEVEL_DEBUG
         @format   = kwargs[:format]
 
-        asl_set_filter(nil, @level) if @level
+        @aslmsg = asl_new(ASL_TYPE_MSG)
+        asl_set(@aslmsg, ASL_KEY_FACILITY, @facility) if @facility
+
+        asl_set_filter(nil, @level)
+      end
+
+      def debug(message)
+        asl_log(nil, @aslmsg, ASL_LEVEL_DEBUG, message)
       end
 
       def info(message)
-        asl_log(nil, nil, ASL_LEVEL_INFO, message)
+        asl_log(nil, @aslmsg, ASL_LEVEL_INFO, message)
+      end
+
+      def info?
+        level >= ASL_LEVEL_INFO
       end
 
       def warn(message)
-        asl_log(nil, nil, ASL_LEVEL_WARNING, message)
+        asl_log(nil, @aslmsg, ASL_LEVEL_WARNING, message)
+      end
+
+      def warn?
+        level >= ASL_LEVEL_WARNING
+      end
+
+      def error
+        asl_log(nil, @aslmsg, ASL_LEVEL_ERR, message)
+      end
+
+      def error?
+        level >= ASL_LEVEL_ERR
+      end
+
+      def fatal
+        asl_log(nil, @aslmsg, ASL_LEVEL_CRIT, message)
+      end
+
+      def fatal?
+        level >= ASL_LEVEL_CRIT
+      end
+
+      def unknown(message)
+        asl_log(nil, @aslmsg, ASL_LEVEL_EMERG, message)
       end
     end
   end
 end
 
 if $0 == __FILE__
-  log = Apple::System::Logger.new(level: Apple::System::Logger::ASL_LEVEL_NOTICE)
-  log.info("GREETINGS DANIEL - INFO")
-  log.warn("GREETINGS DANIEL - WARN")
+  #log = Apple::System::Logger.new(level: Apple::System::Logger::ASL_LEVEL_NOTICE)
+  log = Apple::System::Logger.new(facility: "dberger.test")
+  #p log.info?
+  #p log.warn?
+  #log.info("GREETINGS DANIEL - INFO")
+  log.warn("GREETINGS DANIEL - WARN4")
 end
