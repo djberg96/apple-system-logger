a55f0c75ab0abe5aa8e680fe1e4c7945e785e075
Daniel Berger <djberg96@gmail.com>
2019-10-18 21:04:07 -0400

    Add some methods.

--- lib/apple/system/logger.rb
+++ lib/apple/system/logger.rb
@@ -6,6 +6,32 @@ module Apple
     class Logger
       include Apple::System::LoggerFunctions
       include Apple::System::LoggerConstants
+
+      attr_accessor :facility
+      attr_accessor :level
+      attr_accessor :format
+
+      def initialize(**kwargs)
+        @facility = kwargs[:facility]
+        @level    = kwargs[:level]
+        @format   = kwargs[:format]
+
+        asl_set_filter(nil, @level) if @level
+      end
+
+      def info(message)
+        asl_log(nil, nil, ASL_LEVEL_INFO, message)
+      end
+
+      def warn(message)
+        asl_log(nil, nil, ASL_LEVEL_WARNING, message)
+      end
     end
   end
+end
+
+if $0 == __FILE__
+  log = Apple::System::Logger.new(level: Apple::System::Logger::ASL_LEVEL_NOTICE)
+  log.info("GREETINGS DANIEL - INFO")
+  log.warn("GREETINGS DANIEL - WARN")
 end
