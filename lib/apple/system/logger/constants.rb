07ff7ad07e778bf95581395e543e219fb696e5e2
Daniel Berger <djberg96@gmail.com>
2021-04-13 06:45:02 -0400

    Use single quotes for string literals.

--- lib/apple/system/logger/constants.rb
+++ lib/apple/system/logger/constants.rb
@@ -1,15 +1,15 @@
 module Apple
   module System
     module LoggerConstants
-      ASL_KEY_TIME     = "Time"
-      ASL_KEY_HOST     = "Host"
-      ASL_KEY_SENDER   = "Sender"
-      ASL_KEY_FACILITY = "Facility"
-      ASL_KEY_PID      = "PID"
-      ASL_KEY_UID      = "UID"
-      ASL_KEY_GID      = "GID"
-      ASL_KEY_LEVEL    = "Level"
-      ASL_KEY_MSG      = "Message"
+      ASL_KEY_TIME     = 'Time'
+      ASL_KEY_HOST     = 'Host'
+      ASL_KEY_SENDER   = 'Sender'
+      ASL_KEY_FACILITY = 'Facility'
+      ASL_KEY_PID      = 'PID'
+      ASL_KEY_UID      = 'UID'
+      ASL_KEY_GID      = 'GID'
+      ASL_KEY_LEVEL    = 'Level'
+      ASL_KEY_MSG      = 'Message'
 
       ASL_LEVEL_EMERG   = 0
       ASL_LEVEL_ALERT   = 1
